f9c78447784b853d9e604f0aeecd57ec8763639f
mhjeon <mhjeon@modernlab.io>
2019-02-16 23:42:19 +0900

    Change staticmethod to classmethod
    Staticmethod need to specify class namespace when called within
    create_maze(), as MazeGame.create_maze(). When BombedMazeGame() is
    created BombedMazeGame.create_maze() should be called but it was not
    possible with staticmethod.

--- python-implementation/maze/mazegame_factory_methods.py
+++ python-implementation/maze/mazegame_factory_methods.py
@@ -6,61 +6,57 @@ from wall import Wall, BombedWall
 
 
 class MazeGame:
-    """ Staticmethod: cls나 self에 접근할 수는 없다. regular function처럼 작동하지만
-    class의 namespace에 속해서, 클래스 디자인을 명확하게 하기 위해 보통 사용 """
-
-    @staticmethod
-    def make_maze():
+    @classmethod
+    def make_maze(cls):
         return Maze()
 
-    @staticmethod
-    def make_room(room_number):
+    @classmethod
+    def make_room(cls, room_number):
         return Room(room_number)
 
-    @staticmethod
-    def make_wall():
+    @classmethod
+    def make_wall(cls):
         return Wall()
 
-    @staticmethod
-    def make_door(r1, r2):
+    @classmethod
+    def make_door(cls, r1, r2):
         return Door(r1, r2)
 
-    def create_maze(self):
-        a_maze = MazeGame.make_maze()
-        r1 = MazeGame.make_room(1)
-        r2 = MazeGame.make_room(2)
-        the_door = MazeGame.make_door(r1, r2)
+    def create_maze(cls):
+        a_maze = cls.make_maze()
+        r1 = cls.make_room(1)
+        r2 = cls.make_room(2)
+        the_door = cls.make_door(r1, r2)
 
         a_maze.add_room(r1)
         a_maze.add_room(r2)
 
-        r1.set_side(Direction.NORTH, MazeGame.make_wall())
+        r1.set_side(Direction.NORTH, cls.make_wall())
         r1.set_side(Direction.EAST, the_door)
-        r1.set_side(Direction.SOUTH, MazeGame.make_wall())
-        r1.set_side(Direction.WEST, MazeGame.make_wall())
+        r1.set_side(Direction.SOUTH, cls.make_wall())
+        r1.set_side(Direction.WEST, cls.make_wall())
 
-        r2.set_side(Direction.NORTH, MazeGame.make_wall())
-        r2.set_side(Direction.EAST, MazeGame.make_wall())
-        r2.set_side(Direction.SOUTH, MazeGame.make_wall())
+        r2.set_side(Direction.NORTH, cls.make_wall())
+        r2.set_side(Direction.EAST, cls.make_wall())
+        r2.set_side(Direction.SOUTH, cls.make_wall())
         r2.set_side(Direction.WEST, the_door)
 
         return a_maze
 
 
 class BombedMazeGame(MazeGame):
-
-    @staticmethod
-    def make_wall():
+    @classmethod
+    def make_wall(cls):
         return BombedWall()
 
-    @staticmethod
-    def make_room(room_number):
+    @classmethod
+    def make_room(cls, room_number):
         return RoomWithABomb(room_number)
 
 
 if __name__ == "__main__":
-    # game = MazeGame()
-    # created_maze = game.create_maze()
+    game = MazeGame()
+    created_maze = game.create_maze()
 
     game_with_bombed_maze = BombedMazeGame()
     created_bombed_maze = game_with_bombed_maze.create_maze()
